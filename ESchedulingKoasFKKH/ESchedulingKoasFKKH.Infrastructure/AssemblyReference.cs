0885aa65806e250316c2854574e67ff6487c496a
AdiJT <aditaklal@gmail.com>
2026-04-17 19:17:32 +0800

    Sedikit

---
+++ ESchedulingKoasFKKH/ESchedulingKoasFKKH.Infrastructure/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace ESchedulingKoasFKKH.Infrastructure;
+
+public static class AssemblyReference
+{
+    public readonly static Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
