93881ae296a840ec4353d3a4cb5d0992de46de4d
Lavitzz <allgaming1185@gmail.com>
2026-06-06 20:52:09 +0800

    update font poppins

--- ESchedulingKoasFKKH/eschedulingkoasfkkh.client/tailwind.config.js
+++ ESchedulingKoasFKKH/eschedulingkoasfkkh.client/tailwind.config.js
@@ -32,7 +32,7 @@ export default {
         },
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
+        sans: ['Poppins', 'system-ui', '-apple-system', 'sans-serif'],
       },
       boxShadow: {
         'soft':    '0 2px 15px -3px rgba(0, 0, 0, 0.07), 0 10px 20px -2px rgba(0, 0, 0, 0.04)',
