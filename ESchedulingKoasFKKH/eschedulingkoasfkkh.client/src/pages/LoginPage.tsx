bbd9503d308d524143acd538f51c3466f78b96fe
AdiJT <aditaklal@gmail.com>
2026-09-23 08:49:02 +0800

    Perbaiki logo tidak muncul setelah di publish

--- ESchedulingKoasFKKH/eschedulingkoasfkkh.client/src/pages/LoginPage.tsx
+++ ESchedulingKoasFKKH/eschedulingkoasfkkh.client/src/pages/LoginPage.tsx
@@ -122,7 +122,7 @@ export default function LoginPage() {
           {/* Mobile Logo */}
           <div className="lg:hidden text-center mb-8">
             <div className="w-16 h-16 rounded-2xl flex items-center justify-center mx-auto mb-2">
-              <img className="h-16 object-cover ..." src="src\assets\logo_undana.png" />
+              <img className="h-16 object-cover ..." src={logo} />
             </div>
             <h1 className="text-2xl font-bold text-primary-900">E-Scheduling FKKH</h1>
           </div>
